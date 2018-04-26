3b508b31df754700439c6b7764fc2d255ffe608f
daLong <295528322@qq.com>
2018-04-26 17:53:25 +0800

    添加项目到github

---
+++ qqClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using longLibrary;
+
+namespace qqClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(infoHelper.ShowAuthor());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ qqServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using longLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qqServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(infoHelper.ShowAuthor());
+            Console.ReadKey();
+        }
+    }
+}

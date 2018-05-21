5a35a7e81930f8406a58d2e05ebd961b110bdbb1
banana x <banana@long-pc.BJSLBRA.RNEA.IPTV.MR.ERICSSON.SE>
2018-05-21 14:24:54 +0800

    20180521

---
+++ IO/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region file读写文件
+            //string s = File.ReadAllText(@"C:\Users\banana\Desktop\test.txt", Encoding.UTF8);
+            //Console.WriteLine(s);
+            //File.WriteAllText(@"C:\Users\banana\Desktop\test.txt", "ssssss");
+            //File.AppendAllText(@"C:\Users\banana\Desktop\test.txt", "00000");
+            //Console.WriteLine("done"); 
+            #endregion
+
+
+            #region filestream读取和写入
+            //FileStream fs = new FileStream(@"C:\Users\banana\Desktop\test2.txt", FileMode.Create);
+            //byte[] bytes1 = Encoding.Default.GetBytes("hello\r\n");
+            ////任何数据都是以byte为单位写入的。GetBytes获得字符串的byte表示形式
+            //fs.Write(bytes1, 0, bytes1.Length);
+
+            //byte[] bytes2 = Encoding.Default.GetBytes("www.rupeng.com");
+            //fs.Write(bytes2, 0, bytes2.Length);//在上一个Write的结尾继续写入
+            //fs.Close();//一定要用完了关闭
+
+            //Console.WriteLine("done");
+
+            //using (FileStream fs = new FileStream(@"C:\Users\banana\Desktop\test.txt", FileMode.Open))
+            //{
+
+            //    byte[] bytes = new byte[4];
+            //    //fs.Read(bytes, 0, bytes.Length);//每次读取4个字节，下次Read的时候再读取最多4个byte
+            //    //返回值为真正读取的字节数
+            //    int len;
+
+            //    //len = fs.Read(bytes, 0, bytes.Length);
+            //    //Console.WriteLine(len);
+            //    //len = fs.Read(bytes, 0, bytes.Length);
+            //    //Console.WriteLine(len);
+            //    //len = fs.Read(bytes, 0, bytes.Length);
+            //    //Console.WriteLine(len);
+
+
+            //    //判断是否已经读到了最后
+            //    while ((len = fs.Read(bytes, 0, bytes.Length)) > 0)
+            //    {
+            //        //Console.WriteLine(len);
+            //        //把byte数组转化为字符串
+            //        //string s = Encoding.Default.GetString(bytes);
+            //        string s = Encoding.Default.GetString(bytes, 0, len);
+            //        Console.WriteLine(s);
+            //    }
+
+            //} 
+            #endregion
+
+            #region streamwiter,streamreader读写
+            //using (Stream outStream =
+            //    new FileStream(@"C:\Users\banana\Desktop\test.txt", FileMode.Append))
+            //using (StreamWriter sw =
+            //    new StreamWriter(outStream, Encoding.Default))
+            //{
+            //    sw.Write("hi");
+            //    sw.WriteLine("hehe");
+            //    sw.WriteLine("1111");
+            //}
+
+            //Console.WriteLine("done");
+
+            //using (Stream inStream = 
+            //    new FileStream(@"C:\Users\banana\Desktop\test.txt",FileMode.Open))
+            //using (StreamReader reader =
+            //    new StreamReader(inStream, Encoding.Default))
+            //{
+            //    /*
+            //    string s = reader.ReadToEnd();
+            //    Console.Write(s);*/
+            //    /*
+            //    string s1 = reader.ReadLine();
+            //    Console.WriteLine(s1);
+
+            //    string s2 = reader.ReadLine();
+            //    Console.WriteLine(s2);
+            //        */
+            //    string line;
+            //    while ((line = reader.ReadLine()) != null)
+            //    {
+            //        Console.WriteLine(line);
+            //    }
+            //} 
+            #endregion
+
+            //合并图片
+            //通过图片大小可得知是否成功，新图片大小=老图片1大小+老图片2大小
+            //using (Stream streamReader = new FileStream(@"F:\图片\小新.jpg", FileMode.Open))
+            //using (Stream streamReader2 = new FileStream(@"F:\图片\小和尚.jpeg", FileMode.Open))
+            //using (Stream streamWriter = new FileStream(@"F:\图片\小小新.jpg", FileMode.Create))
+            //{
+            //    byte[] bytes = new byte[1024 * 1024];//1M的缓存
+            //    int length;
+            //    while ((length = streamReader.Read(bytes, 0, bytes.Length)) != 0)
+            //    {
+            //        streamWriter.Write(bytes, 0, length);
+            //    }
+            //    //再次写入需要设置偏移量吗？//不需要，参考合并文本。会自动往后写。
+            //    while ((length = streamReader2.Read(bytes, 0, bytes.Length)) != 0)
+            //    {
+            //        streamWriter.Write(bytes, 0, length);
+            //    }
+            //}
+
+            //合并文本
+            //using (Stream streamReader=new FileStream(@"F:\图片\情书上.txt",FileMode.Open))
+            //using(StreamReader reader=new StreamReader(streamReader,Encoding.Default))
+            //using (Stream streamReader2 = new FileStream(@"F:\图片\情书下.txt", FileMode.Open))
+            //using (StreamReader reader2 = new StreamReader(streamReader2,Encoding.Default))
+            //using(Stream streamWriter=new FileStream(@"F:\图片\情书.txt",FileMode.Create))
+            //using (StreamWriter writer = new StreamWriter(streamWriter, Encoding.Default))
+            //{
+            //    string s;
+            //    while ((s= reader.ReadLine())!=null)
+            //    {
+            //        writer.WriteLine(s);
+            //    }
+            //    while ((s=reader2.ReadLine())!=null)
+            //    {
+            //        writer.WriteLine(s);
+            //    }
+            //}
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
--- regexSerializeXML/Program.cs
+++ regexSerializeXML/Program.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
+
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace regexSerializeXML
 {
@@ -26,21 +30,149 @@ namespace regexSerializeXML
             //}       
             #endregion
 
-            Person person = new Person { Name="long",Age=18};
+            #region serialize
+            //Person person = new Person { Name = "long", Age = 18 };
 
-            BinaryFormatter binaryFormatter = new BinaryFormatter();
-            using (FileStream fs=new FileStream("c:/test/",FileMode.OpenOrCreate))
-            {
-                //StreamWriter
-                //StreamReader
-            }
+            //BinaryFormatter binaryFormatter = new BinaryFormatter();
+            //using (FileStream fs = new FileStream(@"C:\Users\banana\Desktop\test.data", FileMode.OpenOrCreate))
+            //{
+            //    binaryFormatter.Serialize(fs, person);
+            //    Console.WriteLine("done");
+            //}
+            //using (FileStream fs = new FileStream(@"C:\Users\banana\Desktop\test.data", FileMode.OpenOrCreate))
+            //{
+            //    Person person = binaryFormatter.Deserialize(fs) as Person;
+            //    Console.WriteLine(person.Name+" "+person.Age);
+            //}
 
+            //XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+            //using (FileStream fs = new FileStream(@"C:\Users\banana\Desktop\test2.xml", FileMode.OpenOrCreate))
+            //{
+            //    xmlSerializer.Serialize(fs, person);
+            //    Console.WriteLine("done");
+            //} 
+            #endregion
+
+            #region xml读写
+            //xml读取
+            //XmlElement xmlElement = new XmlElement();
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load(@"C:\Users\banana\Desktop\testXml.xml");
+            //XmlNodeList students = doc.DocumentElement.ChildNodes;//Student节点集合
+            //foreach (XmlNode stu in students)
+            //{
+
+            //    XmlElement element = (XmlElement)stu;
+            //    string stuId = element.GetAttribute("StuID");
+            //    XmlNode nameNode = element.SelectSingleNode("StuName");//获取Person节点的Name节点
+            //    string name = nameNode.InnerText;
+            //    Console.WriteLine(stuId + "," + name);
+
+            //}
+
+            //xml写入
+            //Person[] persons = { new Person(1, "rupeng", 8), new Person(2, "baidu", 6) };
+            //XmlDocument doc = new XmlDocument();
+            //XmlElement ePersons = doc.CreateElement("Persons");
+            //doc.AppendChild(ePersons);//添加根节点
+            //foreach (Person person in persons)
+            //{
+            //    XmlElement ePerson = doc.CreateElement("Person");
+            //    ePerson.SetAttribute("id", person.Id.ToString());
+            //    XmlElement eName = doc.CreateElement("Name");
+            //    eName.InnerText = person.Name;
+            //    XmlElement eAge = doc.CreateElement("Age");
+            //    eAge.InnerText = person.Age.ToString();
+            //    ePerson.AppendChild(eName);
+            //    ePerson.AppendChild(eAge);
+            //    ePersons.AppendChild(ePerson);
+            //}
+            //doc.Save(@"C:\Users\banana\Desktop\testWriteXml.xml");
+            //Console.WriteLine("done"); 
+            #endregion
+
+            //查看host和vm
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load(@"C:\Users\banana\Desktop\CNET2SuperlabConfigurationMM_CDVR_5_For3.0.xml");
+
+            //XmlNodeList hosts = doc.GetElementsByTagName("Host");
+
+            //foreach (XmlNode host in hosts)
+            //{
+            //    XmlElement hostElement = (XmlElement)host;
+            //    string hostName = hostElement.GetAttribute("name");
+            //    string hostIpaddress = hostElement.GetAttribute("ipaddress");
+            //    Console.WriteLine(hostName + "|" + hostIpaddress);
+            //    foreach (XmlNode vm in hostElement.ChildNodes)
+            //    {
+            //        XmlElement vmElement = vm as XmlElement;
+            //        List<string> vmInfo = new List<string>();
+            //        string vmName = vmElement.GetAttribute("name");
+            //        string vmIpaddress = vmElement.GetAttribute("ipaddress");
+            //        string baseimage = vmElement.GetAttribute("baseimage");
+            //        string memory = vmElement.GetAttribute("memory");
+            //        string testquery = vmElement.GetAttribute("testquery");
+            //        vmInfo.Add(vmName);
+            //        vmInfo.Add(vmIpaddress);
+            //        vmInfo.Add(baseimage);
+            //        vmInfo.Add(memory);
+            //        vmInfo.Add(testquery);
+            //        //Console.WriteLine("    "+string.Join("|",vmInfo));
+            //    }
+            //}
+
+            //新增host节点
+            //XmlElement newHostElement = doc.CreateElement("Host");
+            //XmlElement templateHost = (XmlElement)hosts.Item(0)
+            //XmlElement newHostElement = (XmlElement)hosts.Item(0).CloneNode(false);
+            //newHostElement.SetAttribute("name", "MRBJ-VHOST-132");
+            //newHostElement.SetAttribute("ipaddress", "nice");
+            //doc.DocumentElement.AppendChild(newHostElement);
+
+
+            //host排序。排序比较困难。。
+            //XmlElement = hosts.Cast<XmlElement>().OrderBy<XmlElement,string>(x=>x.GetAttribute("name"));
+            //doc.InsertAfter()
+
+            //修改host节点
+            //List<XmlElement> hostNodes = new List<XmlElement>();
+            //foreach (XmlNode host in hosts)
+            //{
+            //    hostNodes.Add((XmlElement)host);
+            //}
+
+            //XmlElement hostNode = hostNodes.Where(x => x.GetAttribute("name")== "MRBJ-VHOST-133").Single<XmlElement>();
+            //hostNode.SetAttribute("ipaddress","cool");
+
+
+            //保存
+            doc.Save(@"C:\Users\banana\Desktop\CNET2SuperlabConfigurationMM_CDVR_5_For3.0.xml");
+
+            Console.WriteLine("done");
             Console.ReadKey();
         }
+
     }
+    //[Serializable]
+    //public class Person
+    //{
+    //    public string Name { get; set; }
+    //    public int Age { get; set; }
+    //}
+
     class Person
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
+        public Person(int id, string name, int age)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int Age { set; get; }
     }
+   
+
 }
